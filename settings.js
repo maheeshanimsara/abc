f75431af8c40bb754769541d164c4ca6c1d4abda
maheeshanimsara <maheeshanimsara924@gmail.com>
2025-04-25 09:13:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-
+    
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=S75n0QhB#bwtx_fIp7EsHrKSnApXA8IaLuasfwREFen0oabjYF_M' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
