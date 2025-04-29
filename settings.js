c987f4b995b42b01256df2d1237de4db069d7aba
maheeshanimsara <maheeshanimsara924@gmail.com>
2025-04-29 06:32:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+බ්const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    
+     
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=S75n0QhB#bwtx_fIp7EsHrKSnApXA8IaLuasfwREFen0oabjYF_M' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
